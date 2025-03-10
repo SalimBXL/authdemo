5c3ede222f0033d4c93fec5070e5825d0e7fc080
SalimBXL <el.salim.salim@gmail.com>
2025-03-10 15:45:51 +0100

    Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,9 @@
-class Project < ActiveRecord::Base
+class Project < ApplicationRecord
+  belongs_to :user
   has_many :tasks
   belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true
   validates :user_id, presence: true
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
 end
--- db/migrate/20250223152500_create_projects.rb
+++ db/migrate/20250223152500_create_projects.rb
@@ -7,5 +7,6 @@ class CreateProjects < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
+    add_index :projects, :name, unique: true
   end
 end
